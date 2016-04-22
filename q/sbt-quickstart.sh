3a4d42516c87d9f5b71cbe8fd0e16c2341dd9428
Ufuk Celebi <uce@apache.org>
2016-04-22 18:01:17 +0200

    Update quickstart scripts

--- q/sbt-quickstart.sh
+++ q/sbt-quickstart.sh
@@ -42,7 +42,7 @@ defaultProjectName="Flink Project"
 defaultOrganization="org.example"
 defaultVersion="0.1-SNAPSHOT"
 defaultScalaVersion="2.11.7"
-defaultFlinkVersion="1.0.0"
+defaultFlinkVersion="1.0.2"
 
 echo "This script creates a Flink project using Scala and SBT."
 
