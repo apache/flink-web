17e87da88f573d9ff5afb4cee70b7ae9a0831b53
Till Rohrmann <trohrmann@apache.org>
2016-06-28 15:49:13 +0200

    Update sbt-quickstart.sh script to Flink version 1.1.0

--- q/sbt-quickstart.sh
+++ q/sbt-quickstart.sh
@@ -42,7 +42,7 @@ defaultProjectName="Flink Project"
 defaultOrganization="org.example"
 defaultVersion="0.1-SNAPSHOT"
 defaultScalaVersion="2.11.7"
-defaultFlinkVersion="1.0.3"
+defaultFlinkVersion="1.1.0"
 
 echo "This script creates a Flink project using Scala and SBT."
 
