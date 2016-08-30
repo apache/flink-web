611c5f1be21966011889c0f64791791df8926939
Maximilian Michels <mxm@apache.org>
2016-08-30 15:01:30 +0200

    update build system for docs

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ################################################################################
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
@@ -17,149 +17,42 @@
 # limitations under the License.
 ################################################################################
 
-##
-# Functions
-##
+set -e
+cd "$(dirname ${BASH_SOURCE[0]})"
 
-# set -x
+DIR="`pwd`"
 
+# We need at least bundler to proceed
+if [ "`command -v bundle`" == "" ]; then
+	echo "WARN: Could not find bundle."
+    echo "Attempting to install locally. If this doesn't work, please install with 'gem install bundler'."
 
-# Source: http://stackoverflow.com/a/4025065/568695
-vercomp () {
-    if [[ $1 == $2 ]]
-    then
-        return 0
+    # Adjust the PATH to discover the locally installed Ruby gem
+    if which ruby >/dev/null && which gem >/dev/null; then
+        export PATH="$(ruby -rubygems -e 'puts Gem.user_dir')/bin:$PATH"
     fi
-    local IFS=.
-    local i ver1=($1) ver2=($2)
-    # fill empty fields in ver1 with zeros
-    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
-    do
-        ver1[i]=0
-    done
-    for ((i=0; i<${#ver1[@]}; i++))
-    do
-        if [[ -z ${ver2[i]} ]]
-        then
-            # fill empty fields in ver2 with zeros
-            ver2[i]=0
-        fi
-        if ((10#${ver1[i]} > 10#${ver2[i]}))
-        then
-            return 1
-        fi
-        if ((10#${ver1[i]} < 10#${ver2[i]}))
-        then
-            return 2
-        fi
-    done
-    return 0
-}
 
-# arguments <branch name> <dirName>
-function checkoutDocsForVersionInBranch() {
-    BRANCH=$1
-    DIR=$2
-    echo "Checking out docs from branch '$BRANCH' into dir '$DIR' "
-
-    cd flink-src-repo
-    git checkout $BRANCH
-    git clean -d -f
-
-    cd docs
-
-    # we are in _root_/flink-src-repo/docs
-    export BUILD_API=1
-    jekyll build  --destination ../../docs/$DIR
-
-    # Go back to root of flink website
-    cd ../../
-}
-
-# no args
-function updateDocsDir() {
-    echo "Clone if necessary"
-    if [ ! -d "flink-src-repo" ]; then
-        git clone http://git-wip-us.apache.org/repos/asf/flink.git flink-src-repo
-    fi
-    if [ ! -d "docs" ]; then
-        mkdir docs
-    fi
-    echo "Fetch rep"
-    cd flink-src-repo
-    git fetch
-    cd ..
-
-    echo "Create docs for versions"
-#   checkoutDocsForVersionInBranch "documentation" "0.5"
-    checkoutDocsForVersionInBranch "origin/release-0.8" "0.8"
-    checkoutDocsForVersionInBranch "origin/release-0.7" "0.7-incubating"
-    checkoutDocsForVersionInBranch "origin/release-0.6.2" "0.6-incubating"
-}
-
-##
-# Main body
-##
-
-
-HAS_JEKYLL=true
-
-command -v jekyll > /dev/null
-if [ $? -ne 0 ]; then
-    echo -n "ERROR: Could not find jekyll. "
-    echo "Please install with 'gem install jekyll' (see http://jekyllrb.com)."
-
-    HAS_JEKYLL=false
+    # install bundler locally
+    gem install --user-install bundler
 fi
 
-JEKYLL_VERSION=`jekyll --version | cut -d' ' -f2`
-vercomp $JEKYLL_VERSION "2.1.0"
+# Install Ruby dependencies locally
+bundle install --path .rubydeps
 
-if [ "$?" == "2" ]; then
-    echo "Please use at least jekyll version 2.1.0 instead of '$JEKYLL_VERSION'"
-    exit 1
-fi
-
-command -v redcarpet > /dev/null
-if [ $? -ne 0 ]; then
-    echo -n "WARN: Could not find redcarpet. "
-    echo -n "Please install with 'sudo gem install redcarpet' (see https://github.com/vmg/redcarpet). "
-    echo "Redcarpet is needed for Markdown parsing and table of contents generation."
-fi
-
-command -v pygmentize > /dev/null
-if [ $? -ne 0 ]; then
-    echo -n "WARN: Could not find pygments. "
-    echo -n "Please install with 'sudo easy_install pygments' (requires Python; see http://pygments.org). "
-    echo "Pygments is needed for syntax highlighting of the code examples."
-fi
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-SRC=${DIR}
-DST=${SRC}/content
+DOCS_SRC=${DIR}
+DOCS_DST=${DOCS_SRC}/content
 
 # default jekyll command is to just build site
 JEKYLL_CMD="build"
 
-
-
-OPTIND=1
 # if -p flag is provided, serve site on localhost
-while getopts ":up" opt; do
-    case $opt in
-        p)
-            JEKYLL_CMD="serve --watch"
-            ;;
-        u)
-            echo "updating docs from repository"
-            updateDocsDir
-            ;;
-    esac
+while getopts ":p" opt; do
+	case $opt in
+		p)
+		JEKYLL_CMD="serve --baseurl= --watch"
+		;;
+	esac
 done
 
-# integrate documentation
-
-if $HAS_JEKYLL; then
-    jekyll ${JEKYLL_CMD} --source ${SRC} --destination ${DST}
-fi
+# use 'bundle exec' to insert the local Ruby dependencies
+bundle exec jekyll ${JEKYLL_CMD} --source "${DOCS_SRC}" --destination "${DOCS_DST}"
