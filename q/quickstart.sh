514c8f010e524e5d1972dd4a95ce7b9d7363b0a5
Roman Khachatryan <khachatryan.roman@gmail.com>
2021-02-16 19:24:31 +0100

    Revert "Add Apache Flink 1.12.2 release"
    This reverts commit 5da7ae9e1f7ee786cf1e3e209cbdb37863567f28.

--- q/quickstart.sh
+++ q/quickstart.sh
@@ -24,7 +24,7 @@ PACKAGE=quickstart
 mvn archetype:generate								\
   -DarchetypeGroupId=org.apache.flink				\
   -DarchetypeArtifactId=flink-quickstart-java		\
-  -DarchetypeVersion=${1:-1.12.2}							\
+  -DarchetypeVersion=${1:-1.12.1}							\
   -DgroupId=org.myorg.quickstart					\
   -DartifactId=$PACKAGE								\
   -Dversion=0.1										\
