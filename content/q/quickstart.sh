734810cdb8a1c94f9af1b5ee9c94a5f1fba0030c
Dawid Wysakowicz <dwysakowicz@apache.org>
2021-05-28 18:39:59 +0200

    Rebuild webpage

--- content/q/quickstart.sh
+++ content/q/quickstart.sh
@@ -24,7 +24,7 @@ PACKAGE=quickstart
 mvn archetype:generate								\
   -DarchetypeGroupId=org.apache.flink				\
   -DarchetypeArtifactId=flink-quickstart-java		\
-  -DarchetypeVersion=${1:-1.13.0}							\
+  -DarchetypeVersion=${1:-1.13.1}							\
   -DgroupId=org.myorg.quickstart					\
   -DartifactId=$PACKAGE								\
   -Dversion=0.1										\
