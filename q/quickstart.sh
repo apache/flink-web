672b57a73162fe69277eb08977e3d5f7868affe8
Stephan Ewen <sewen@apache.org>
2021-09-10 09:02:07 +0200

    Add Apache Flink release 1.14.0 Blog post and update links and scripts.
    This closes #468
    
    Co-authored-by: Joe Moser <joe@ververica.com>
    Co-authored-by: Daisy T <daisyts@gmx.com>

--- q/quickstart.sh
+++ q/quickstart.sh
@@ -24,7 +24,7 @@ PACKAGE=quickstart
 mvn archetype:generate								\
   -DarchetypeGroupId=org.apache.flink				\
   -DarchetypeArtifactId=flink-quickstart-java		\
-  -DarchetypeVersion=${1:-1.13.2}							\
+  -DarchetypeVersion=${1:-1.14.0}							\
   -DgroupId=org.myorg.quickstart					\
   -DartifactId=$PACKAGE								\
   -Dversion=0.1										\
--- q/sbt-quickstart.sh
+++ q/sbt-quickstart.sh
@@ -42,7 +42,7 @@ defaultProjectName="Flink Project"
 defaultOrganization="org.example"
 defaultVersion="0.1-SNAPSHOT"
 defaultScalaVersion="2.12.7"
-defaultFlinkVersion="1.13.2"
+defaultFlinkVersion="1.14.0"
 
 echo "This script creates a Flink project using Scala and SBT."
 
@@ -105,7 +105,7 @@ organization := \"org.example\"
 
 ThisBuild / scalaVersion := \"2.11.12\"
 
-val flinkVersion = \"1.13.2\"
+val flinkVersion = \"1.14.0\"
 
 val flinkDependencies = Seq(
   \"org.apache.flink\" %% \"flink-scala\" % flinkVersion % \"provided\",
