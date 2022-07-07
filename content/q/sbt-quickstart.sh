dd81767ff4d7b7272d3fd35ae4db08db8ea986aa
David Anderson <david@alpinegizmo.com>
2022-07-07 18:01:21 -0400

    rebuild website

--- content/q/sbt-quickstart.sh
+++ content/q/sbt-quickstart.sh
@@ -42,7 +42,7 @@ defaultProjectName="Flink Project"
 defaultOrganization="org.example"
 defaultVersion="0.1-SNAPSHOT"
 defaultScalaVersion="2.12.7"
-defaultFlinkVersion="1.15.0"
+defaultFlinkVersion="1.15.1"
 
 echo "This script creates a Flink project using Scala and SBT."
 
